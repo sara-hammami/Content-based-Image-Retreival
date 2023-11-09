99c54ce64e726d7a05388b7eac17af1529eb4a4e
sarra <sarra.hammami456@gmail.com>
2023-11-09 11:35:34 +0100

    update docs

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from torchvision import transforms
 from src.dataloader import get_transformation
 import os 
 from flask import send_file
-
+import requests
 app = Flask(__name__)
 es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
 UPLOAD_FOLDER = 'static/uploads'
@@ -53,52 +53,44 @@ def search_similar_images(feature_vector, index_name='cbirproject'):
 
     return similar_images
 
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    similar_images = None
+    text_search_results = None
+    
+
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return render_template('index.html', error='No file part')
-        
-        file = request.files['file']
-        
-        if file.filename == '':
-            return render_template('index.html', error='No selected file')
-        
-        if file and allowed_file(file.filename):
-            # Save the uploaded file
-            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filename)
+        if 'file' in request.files:
+            # Handle image upload logic
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                # Save the uploaded file
+                filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(filename)
 
-            #Load, transform, and extract features from the uploaded image
-            image = Image.open(filename)
-            transform=get_transformation()
-            image_tensor=transform(image)
-            image_tensor = image_tensor.unsqueeze(0).to(device)
-            feature = resnet_model.extract_features(image_tensor)
+                # Load, transform, and extract features from the uploaded image
+                image = Image.open(filename)
+                transform = get_transformation()
+                image_tensor = transform(image)
+                image_tensor = image_tensor.unsqueeze(0).to(device)
+                feature = resnet_model.extract_features(image_tensor)
 
-           # Search for similar_images in Elasticsearch
-            similar_images = search_similar_images(feature)
-            return render_template('index.html', image_path=filename, similar_images=similar_images)
-
-    return render_template('index.html', error=None, image_path=None, similar_images=None ) ,{'Content-Language': 'en'}
-@app.route('/images/<path:image_path>')
-def serve_image(image_path):
-    return send_file(image_path, mimetype='image/jpeg')# Adjust mimetype if necessary
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    if request.method == 'POST':
-        # Check if the search query is in the form data
-        if 'search_query' in request.form:
+                # Search for similar_images in Elasticsearch
+                similar_images = search_similar_images(feature)
+        elif 'search_query' in request.form:
+            # Handle text search logic
             search_term = request.form['search_query']
             # Execute Elasticsearch search query for text search
             res = es.search(
-                index="flickerphotos", 
-                size=30, 
+                index="flickerphotos",
+                size=30,
                 body={
                     "query": {
                         "multi_match": {
-                            "query": search_term, 
+                            "query": search_term,
                             "fields": ["url", "title", "tags"],
                             "fuzziness": "AUTO",
                             "prefix_length": 1
@@ -106,15 +98,14 @@ def search():
                     }
                 }
             )
-            # Render the template with text search results
-            return render_template('index.html', res=res)
+            
+            # Extract text search results
+            text_search_results = res['hits']['hits']
+            
 
-    # Handle other cases (GET request, no search query, etc.)
-    return render_template('index.html')
+    return render_template('index.html', similar_images=similar_images, text_search_results=text_search_results)
+@app.route('/images/<path:image_path>')
+def serve_image(image_path):
+    return send_file(image_path, mimetype='image/jpeg')# Adjust mimetype if necessary
 if __name__ == '__main__':
-<<<<<<< Updated upstream
     app.run(debug=True)
-
-=======
-    app.run(debug=True)
->>>>>>> Stashed changes
